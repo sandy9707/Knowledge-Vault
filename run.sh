aaab2f2669a486b1a83a7ea16ea381345eb05c5e
sandy9707 <Sandy970701@Gmail.com>
2023-06-18 17:10:39 +0800

    2023-06-18 17-10-39

--- run.sh
+++ run.sh
@@ -10,3 +10,7 @@ PATH=$PATH:/opt/homebrew/bin
 tree $SHELL_FOLDER -H https://sandy9707.github.io/Knowledge-Vault/ -o index.html
 
 
+git add .
+commitTime=`date +"%Y-%m-%d %H-%M-%S"`
+git commit -a -m "${commitTime}"
+git push origin main
