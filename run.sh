0902f860ed78cdd56edc0f7503fb1449b32362b2
Tenney Wood <tenney@Tenneys-MacBook-Air.local>
2023-10-01 16:13:04 +0800

    2023-10-01 16-13-03

--- run.sh
+++ run.sh
@@ -3,9 +3,17 @@
 # 获取当前脚本所在目录，并输出
 SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
 echo $SHELL_FOLDER
-LC_ALL=en_US.UTF-8
-LANG=en_US.UTF-8
-_=/usr/bin/env
+
+
+# set environment variables
+# 检查 env.txt 文件是否存在
+if [ -f "${HOME}/env.txt" ]; then
+  # 如果 env.txt 存在，则运行相应的命令
+  cat ${HOME}/env.txt | xargs -n 1 -I {} sh -c 'export {}'
+  # 运行其他命令
+else
+  echo "env.txt 文件不存在"
+fi
 
 # tree命令生成目录树
 cd $SHELL_FOLDER
