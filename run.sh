83ff0f7a8371cbe2ad933971602cd388817ed23f
sandy9707 <Sandy970701@Gmail.com>
2023-10-05 19:39:04 +0800

    2023-10-05 19-39-04

--- run.sh
+++ run.sh
@@ -5,15 +5,19 @@ SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
 echo $SHELL_FOLDER
 
 
-# set environment variables
+# Set environment variables
 # 检查 env.txt 文件是否存在
 if [ -f "${HOME}/env.txt" ]; then
   # 如果 env.txt 存在，则运行相应的命令
-  cat ${HOME}/env.txt | xargs -n 1 -I {} sh -c 'export {}'
-  # 运行其他命令
+  while IFS='=' read -r key value; do
+    echo "$key=$value"
+    export "$key"="$value"
+  done < "${HOME}/env.txt"
 else
+  # 运行其他命令
   echo "env.txt 文件不存在"
 fi
+
 
 # tree命令生成目录树
 cd $SHELL_FOLDER
